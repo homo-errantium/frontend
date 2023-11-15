edd24242620a25417041ddb50a8ef3f4ddf6c7bf
LEH1CH <usman0val@vk.com>
2023-11-15 17:50:16 +0300

    refactor: correction after the review

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import './NotFound.scss'
-import NotFoundImage from '../../stories/assets/404.png'
+import NotFoundImage from '../../img/404.png'
 import Header from '../../stories/Header'
 
 export default function NotFound() {
 	return (
-		<section>
+		<>
 			<Header />
-			<div className="error">
-				<img className="error__image" alt="404" src={NotFoundImage} />
+			<section className="error">
+				<img
+					className="error__image"
+					alt="изображение отсутствующей страницы"
+					src={NotFoundImage}
+				/>
 				<p className="error__subtitle">
 					Извините, запрашиваемая страница не найдена. Возможно, она
 					перемещена, удалена или вы ввели неверный адрес.
 				</p>
-			</div>
-		</section>
+			</section>
+		</>
 	)
 }
