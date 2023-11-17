240f6f2143023d7c1bd4d63608ea46a728285806
Irina Babaeva <irina.a.babaeva@yandex.ru>
2023-11-17 13:24:48 +0300

    feat(resume components): addButton, FormInput and ResumeTitle were added to Resume

---
+++ src/components/Resume/ResumeComponents/AddButton/AddButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './AddButton.scss'
+
+const AddButton = () => (
+	<button className="add-button link" type="button">
+		+ Добавить
+	</button>
+)
+
+export default AddButton
---
+++ src/components/Resume/ResumeComponents/FormInput/FormInput.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import './FormInput.scss'
+import Tip from '../../Tip/Tip'
+
+const FormInput = ({ label, tip, tipText, extraInputClass }) => (
+	<div className="form-input">
+		<div className="form-input__label-container">
+			<label className="form-input__label" htmlFor="form-input">
+				{label}
+			</label>
+			{tip && <Tip text={tipText} />}
+		</div>
+		<input
+			id="form-input"
+			className={classNames(
+				'form-input__field',
+				extraInputClass && `form-input__field_${extraInputClass}`
+			)}
+			type="text"
+		/>
+	</div>
+)
+FormInput.propTypes = {
+	label: PropTypes.node.isRequired,
+	tip: PropTypes.bool,
+	tipText: PropTypes.string,
+	extraInputClass: PropTypes.string,
+}
+
+FormInput.defaultProps = {
+	tip: false,
+	tipText: '',
+	extraInputClass: '',
+}
+
+export default FormInput
