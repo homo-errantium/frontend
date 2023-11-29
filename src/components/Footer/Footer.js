1b161e7ec2f2f203e06199eddcbb9c2c1f3ce4f8
Irina Babaeva <irina.a.babaeva@yandex.ru>
2023-11-29 17:11:08 +0300

    feat(footer): footer updated in according to mvp layput

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,20 @@
 import './Footer.scss'
+import ResumeLogo from '../../img/resume.svg'
+import PlusLogo from '../../img/plus-logo.svg'
 
 function Footer() {
-  const date = new Date()
-  const thisYear = date.getFullYear()
   return (
     <footer className="footer">
-      <p className="footer__author">&copy; {thisYear}. Яндекс.Practicum</p>
+      <div className="footer__flex-container">
+        <div className="footer__logo">
+          <img alt="резюме" src={ResumeLogo} className="footer__logo-resume" />
+          <img alt="плюс" src={PlusLogo} className="footer__logo-plus" />
+        </div>
+        <div className="footer__contacts">
+          <span className="footer__contact">8 (800) 555 35 35</span>
+          <span className="footer__contact">pochta@gmail.com</span>
+        </div>
+      </div>
     </footer>
   )
 }
